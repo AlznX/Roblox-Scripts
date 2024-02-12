667acd126ea3dbf4e3095c02f4db0f53d846bc9c
Alzn's Github <105345112+AlznX@users.noreply.github.com>
2024-02-11 21:23:30 -0800

    Update Admin Detection.lua

--- Admin Detection.lua
+++ Admin Detection.lua
@@ -4,10 +4,18 @@ local name = "BAiLeymen"
 game:GetService'Players'.PlayerAdded:Connect(function(player)
 game.Players.LocalPlayer:Kick("The co-owner, has joined the server you have been kicked from the game so you will not get banned and possibly, reported please do not try to reload the script while they're in there you may get banned.")
 end)
-
 pcall(function()
 local name = "Firemalte07" 
 game:GetService'Players'.PlayerAdded:Connect(function(player)
 game.Players.LocalPlayer:Kick("The owner, has joined the server you have been kicked from the game so you will not get banned and possibly, reported please do not try to reload the script while they're in there you may get banned.")
 
+end)
+
+wait(2)
+local name = "Jay1" 
+game:GetService'Players'.PlayerAdded:Connect(function(player)
+game.Players.LocalPlayer:Kick("Sorry for the inconvenience but this script has unfortunately crashed please reload and try again later.")
+print("loadded detection.")
+end)
+end)
 end)
