0e7ff4da29ddc7f02d2a940d2bf755c9e8bd14cf
Alzn's Github <105345112+AlznX@users.noreply.github.com>
2024-02-11 19:47:43 -0800

    Update Admin Detection.lua

--- Admin Detection.lua
+++ Admin Detection.lua
@@ -1,18 +1,20 @@
 --// This was created for sword simulator by Cyvex --//
-
+pcall(function()
 local name = "BAiLeymen" 
 game:GetService'Players'.PlayerAdded:Connect(function(player)
 game.Players.LocalPlayer:Kick("The co-owner, has joined the server you have been kicked from the game so you will not get banned and possibly, reported please do not try to reload the script while they're in there you may get banned.")
 end)
 
-wait(3)
+
 local name = "Firemalte07" 
 game:GetService'Players'.PlayerAdded:Connect(function(player)
 game.Players.LocalPlayer:Kick("The owner, has joined the server you have been kicked from the game so you will not get banned and possibly, reported please do not try to reload the script while they're in there you may get banned.")
 print("loadded detection.")
 end)
 
+
 local name = "robloxmaster30931" 
 game:GetService'Players'.PlayerAdded:Connect(function(player)
 game.Players.LocalPlayer:Kick("Sorry for the inconvenience but this script has unfortunately crashed please reload and try again later.")
+end)
 end)
