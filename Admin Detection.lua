1b51708eb35d0e3d0cf821d9dd9045001d24a8fa
Alzn's Github <105345112+AlznX@users.noreply.github.com>
2024-02-13 20:54:59 -0500

    Delete Admin Detection.lua

--- Admin Detection.lua
+++
@@ -1,5 +0,0 @@
---// This was created for sword simulator by Cyvex --//
-local name = "EzPower221" 
-game:GetService'Players'.PlayerAdded:Connect(function(player)
-game.Players.LocalPlayer:Kick("Admin Detected: BAiLeymen, has joined you server you have been kicked from the game so you will not get banned please do not try to use the script while they're in the game.")
-end)
