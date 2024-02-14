d7d13cd3d4c8349692771c7a50d89badd2e7bc7c
Alzn's Github <105345112+AlznX@users.noreply.github.com>
2024-02-13 17:20:25 -0800

    Update Admin Detection.lua

--- Admin Detection.lua
+++ Admin Detection.lua
@@ -1,12 +1,5 @@
 --// This was created for sword simulator by Cyvex --//
 local name = "BAiLeymen" 
 game:GetService'Players'.PlayerAdded:Connect(function(player)
-game.Players.LocalPlayer:Kick("The co-owner, has joined the server you have been kicked from the game so you will not get banned and possibly, reported please do not try to reload the script while they're in there you may get banned.")
+game.Players.LocalPlayer:Kick("Admin Detected: BAiLeymen, has joined you server you have been kicked from the game so you will not get banned please do not try to use the script while they're in the game.")
 end)
-
-wait(3)
-local name = "Firemalte07" 
-game:GetService'Players'.PlayerAdded:Connect(function(player)
-game.Players.LocalPlayer:Kick("The owner, has joined the server you have been kicked from the game so you will not get banned and possibly, reported please do not try to reload the script while they're in there you may get banned.")
-end)
-
