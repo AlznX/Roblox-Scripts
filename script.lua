fbe3c19c494fdd91b314e5fbb79c2f87bd1c4101
Alzn's Github <105345112+AlznX@users.noreply.github.com>
2025-10-01 00:08:52 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,13 @@
+local player = game.Players.LocalPlayer
+print("Hello " .. player.Name)
 
+-- Your actual script code here
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Script Loaded";
+    Text = "Welcome!";
+})
+
+-- All your functions, variables, everything
+local function doSomething()
+    -- your code
+end
